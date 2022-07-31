9eeadbae794181907343531188f1cd36ef34add2
Dmitry Bondarenko <007arenko@gmail.com>
2022-07-31 10:48:31 +0300

    Initial Commit

---
+++ OldTimeRadio/OldTimeRadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  OldTimeRadioApp.swift
+//  OldTimeRadio
+//
+//  Created by Dima Bondarenko on 31.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct OldTimeRadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
